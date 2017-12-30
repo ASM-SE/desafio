55aea64e3d7705b87e2c2d0ca5ddf73d84e79d13
ASM-SE <andersonmarcolino@gmail.com>
2017-12-30 10:54:51 -0200

    Create start button and make adjustment on restart button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DomSanitizer  } from '@angular/platform-browser';
 
+import { Player } from '../../schemas/player.schema';
+
 @Component({
   selector: 'app-button-player',
   templateUrl: './button.component.html',
@@ -22,24 +24,32 @@ export class ButtonComponent implements OnInit {
   }
 
   private action(data : any) : void{
+
     if(data.name){    
       this.event.emit(this.data);
-    }else{
-      this.event.emit(true);    
+    }else if(data === 'Restart'){
+      this.event.emit('restart');    
+    }else if(data === 'Start'){
+      this.event.emit('start'); 
     }
 
   }
 
   private setButtonOptions(data : any) {
+    console.log(data);
       //setting button text
       if(data.name){    
         this.buttonText = this.data.name;
         const classStyle = `btn btn-primary`;
         return this.sanitizer.bypassSecurityTrustStyle(classStyle);
-      }else{
+      }else if(data === 'Restart'){
         this.buttonText = this.data;
         const classStyle = `btn btn-danger`;
         return this.sanitizer.bypassSecurityTrustStyle(classStyle);        
+      }else  if(data === 'Start'){
+        this.buttonText = this.data;
+        const classStyle = `btn btn-success`;
+        return this.sanitizer.bypassSecurityTrustStyle(classStyle);         
       }
   }
 
--- src/app/components/game-screen/game-screen.component.ts
+++ src/app/components/game-screen/game-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 
 
@@ -13,6 +13,8 @@ import { life, damage } from '../../shared/services/shared.service';
 })
 export class GameScreenComponent implements OnInit {
 
+
+  //Setting players data
   public playerOne : Player = {
     id: 'playerone',
     name: 'Player 1',
@@ -29,36 +31,81 @@ export class GameScreenComponent implements OnInit {
     defeats: 0    
   }
 
+  //Variables
   public restart : string = 'Restart';
-
-  private winner : string = '';
+  private winner : string = null;
+  private showGameScreen : Boolean = false;
+  private gameStart : Boolean = false;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
-  public makeAction(eventdata: any){
-     
-      if(eventdata != true){
+  //Listener for keybord press to make damage in the oposite player
+  @HostListener('window:keypress', ['$event']) keyEvent(event: KeyboardEvent){
 
-        if((eventdata.id === this.playerOne.id) && (this.playerTwo.life > 0) && (this.playerOne.life > 0)){ 
-          this.playerTwo.life -= damage;
-          if(this.playerTwo.life === 0){
-            this.winner = this.playerOne.name + ' Win!';
-          }
-        }else if((eventdata.id === this.playerTwo.id) && (this.playerOne.life > 0) && (this.playerTwo.life > 0)){
-          this.playerOne.life -= damage;
-          if(this.playerOne.life === 0){
-            this.winner = this.playerTwo.name + ' Win!';
-          }
+        if ((event.key === "s")&&(!this.winner)) {
+
+          this.makeAction(this.playerOne);
+
+        }else if ((event.key === "l")&&(!this.winner)) {
+
+          this.makeAction(this.playerTwo);
+
         }
 
-      }else{
+  }
+  
+
+  /* Button action function according to the eventdata parameter value:
+                 (1) if is player data, make damage or, 
+                 (2) if is true value restart the game.*/
+  public makeAction(eventdata: any){
+     
+      if((eventdata != 'start')&&(eventdata != 'restart')){
+
+        if((eventdata.id === this.playerOne.id) && (this.playerTwo.life > 0) && (this.playerOne.life > 0)){ 
+          
+          this.playerTwo.life -= damage;
+          
+          if(this.playerTwo.life === 0){
+              this.winner = this.playerOne.name + ' Win!';
+              this.showGameScreen = false;
+          }
+
+        }else if((eventdata.id === this.playerTwo.id) && (this.playerOne.life > 0) && (this.playerTwo.life > 0)){
+          
+          this.playerOne.life -= damage;
+
+          if(this.playerOne.life === 0){
+              this.winner = this.playerTwo.name + ' Win!';
+              this.showGameScreen = false;              
+          }
+          
+        }
+
+      }else if(eventdata === 'restart'){
         
-        this.playerOne.life = life;
-        this.playerTwo.life = life;
-        this.winner = '';
+
+        if(this.winner != null){
+          this.playerOne.life = life;
+          this.playerTwo.life = life;
+          this.winner = null;   
+          this.showGameScreen = true; 
+          this.gameStart = true; 
+        }else{
+          this.playerOne.life = life;
+          this.playerTwo.life = life;
+          this.winner = null;   
+          this.showGameScreen = true; 
+          this.gameStart = true; 
+        }   
+
+
+      }else if(eventdata === 'start'){
+
+        this.showGameScreen = true;
+        this.gameStart = true;
 
       }
 
